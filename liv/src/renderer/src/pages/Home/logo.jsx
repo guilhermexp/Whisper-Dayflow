7eb7129cbab888c652d11c660886745258ea5283
guilhermexp <guilherme-varela@hotmail.com>
2026-03-13 21:34:29 -0300

    Revisar app e corrigir bugs

--- liv/src/renderer/src/pages/Home/logo.jsx
+++
@@ -1,7 +0,0 @@
-// Pile logo component
-import React from 'react';
-import { PileIcon } from 'renderer/icons';
-
-export default function Logo({ size = 32 }) {
-  return <PileIcon size={size} />;
-}
--- liv/src/shared/shims.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    APP_ID: string
-    APP_VERSION: string
-    PRODUCT_NAME: string
-    IS_MAC: boolean
-  }
-}
